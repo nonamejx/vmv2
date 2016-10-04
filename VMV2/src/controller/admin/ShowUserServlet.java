d395ab4bebfe5310903e66710beebd4f5e5d6ea7
hhhhuy112 <minhhuyho2011@gmail.com>
2016-10-04 16:15:57 +0700

    feature-user_management-update_user_-huyhm-041016

---
+++ VMV2/src/controller/admin/ShowUserServlet.java
@@ -0,0 +1,65 @@
+package controller.admin;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.Gson;
+
+import model.bean.News;
+import model.bean.User;
+import model.bo.NewsBO;
+import model.bo.UserBO;
+import utils.DateUtils;
+
+/**
+ * Servlet implementation class ShowNewsServet
+ */
+@WebServlet("/ShowUserServlet")
+public class ShowUserServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public ShowUserServlet() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		doPost(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		UserBO userBO = new UserBO();
+		String userIdStr = request.getParameter("userId");
+		int userId = 0;
+		if (userIdStr != null) {
+			userId = Integer.parseInt(userIdStr);
+		}
+		User user = userBO.getUserById(userId);
+		String json = new Gson().toJson(user);
+		System.out.println(json);
+		response.setContentType("application/json");
+		response.setCharacterEncoding("UTF-8");
+		response.getWriter().write(json);
+
+	}
+
+}
